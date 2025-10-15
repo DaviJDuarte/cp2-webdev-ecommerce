7cf8054ba3c16ecda4ee6ec02a71911c27255c2b
Davi Duarte <davijduarte.dev@gmail.com>
2025-10-14 21:39:00 -0300

    Commit para não perder tudo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home'
+import Layout from "./Layout";
+
+const router = createBrowserRouter([{
+  path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+    ],
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const API = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    fetch(`${API}/products`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((err) => console.error(err));
+  }, [API]);
   
   return (
     <div>
         <h2 className="text-2xl font-bold mb-4">Bem-vindo ao Fiap Commerce!</h2>
+        {products.map((product) => <p key={product.id}>{product.id}</p>)}
     </div>
   );
 }
